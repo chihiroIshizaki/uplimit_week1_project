6c26d43eff2e5d5e5cd615a28d62e8a3a0a34753
chihiroIshizaki <95729805+chihiroIshizaki@users.noreply.github.com>
2023-08-23 12:27:51 +0900

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -36,17 +36,13 @@ def main():
 
         with col2:
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
-
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
+            
+       # Display related facts about the topic
+        st.subheader("an extra about the topic")
+        related_info = podcast_info['podcast_guest']
+        for funfact in related_info.split('\n'):
+            st.markdown(
+                f"<p style='margin-bottom: 5px;'>{funfact}</p>", unsafe_allow_html=True)
 
         # Display the five key moments
         st.subheader("Key Moments")
